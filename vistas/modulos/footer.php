75292f3e28b6465072fd58880b12b9b2a6f9fc32
surynam <119618856+SuryNamtz@users.noreply.github.com>
2025-03-09 22:42:45 -0600

    Commit inicial: Subo mi proyecto a GitHub

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2025 <a href="https://www.facebook.com/inversionesmicasitahn/" target="_blank">Inversiones mi Casita</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>

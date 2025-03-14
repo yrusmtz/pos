1284ca1a3be46dd4d40732b116a49848bc30c656
Sury Nayely Martinez <79479231+yrusmtz@users.noreply.github.com>
2025-03-13 21:45:24 -0600

    fix: agrega inicio al sistema y roles de usuario

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -4,7 +4,11 @@
 
 		<ul class="sidebar-menu">
 
-			<li class="active">
+		<?php
+
+		if($_SESSION["perfil"] == "Administrador"){
+
+			echo '<li class="active">
 
 				<a href="inicio">
 
@@ -24,9 +28,13 @@
 
 				</a>
 
-			</li>
+			</li>';
 
-			<li>
+		}
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Especial"){
+
+			echo '<li>
 
 				<a href="categorias">
 
@@ -46,9 +54,13 @@
 
 				</a>
 
-			</li>
+			</li>';
 
-			<li>
+		}
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li>
 
 				<a href="clientes">
 
@@ -57,9 +69,13 @@
 
 				</a>
 
-			</li>
+			</li>';
 
-			<li class="treeview">
+		}
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li class="treeview">
 
 				<a href="#">
 
@@ -97,9 +113,11 @@
 
 						</a>
 
-					</li>
+					</li>';
 
-					<li>
+					if($_SESSION["perfil"] == "Administrador"){
+
+					echo '<li>
 
 						<a href="reportes">
 							
@@ -108,11 +126,19 @@
 
 						</a>
 
-					</li>
+					</li>';
 
-				</ul>
+					}
 
-			</li>
+				
+
+				echo '</ul>
+
+			</li>';
+
+		}
+
+		?>
 
 		</ul>
 
